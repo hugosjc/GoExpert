f774341078d64cfbd2a28c87ca7de71a4d5d75fd
hugosjc <hugo.alexandro1@gmail.com>
2023-03-13 06:08:48 -0300

    Declaração e atribuição

---
+++ 2/main.go
@@ -0,0 +1,21 @@
+package main
+
+const a = "Hello, World"
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "Hugo"
+	e float64 = 1.2
+)
+
+func main() {
+	a := "X" //string
+	a = "fd"
+
+	println(a)
+}
+
+func x() {
+
+}
