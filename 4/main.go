fdff62be83b1f88408ccf6e092bc92802b1af356
hugosjc <hugo.alexandro1@gmail.com>
2023-03-14 05:32:43 -0300

    Importando fmt e tipagem

---
+++ 4/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type ID int
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "Hugo"
+	e float64 = 1.2
+	f ID      = 1
+)
+
+func main() {
+	fmt.Printf("O tipo de E é %T", f)
+}
