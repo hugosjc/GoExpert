1b6d8863325058d8a9eb7380b5526c02720e704c
hugosjc <hugo.alexandro1@gmail.com>
2023-03-16 05:47:35 -0300

    Funções variádicas

--- fundacao/funcoes_variadicas/main.go
+++ fundacao/funcoes_variadicas/main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 
-	fmt.Println(sum(1, 2, 3, 5, 4, 5, 4, 84, 78, 48, 5, 4))
+	fmt.Println(sum(1, 2, 3, 5, 4, 5, 4, 84, 78, 48, 5, 4, 100))
 }
 
 func sum(numeros ...int) int {
