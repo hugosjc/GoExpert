a2f2cc4a6ebf847ccd56dfdb41b4a5dc3bd26ba2
hugosjc <hugo.alexandro1@gmail.com>
2023-03-24 06:03:21 -0300

    Structs

---
+++ fundacao/structs/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type Cliente struct {
+	Nome  string
+	Idade int
+	Ativo bool
+}
+
+func main() {
+	hugo := Cliente{
+		Nome:  "Hugo",
+		Idade: 38,
+		Ativo: true,
+	}
+	hugo.Ativo = false
+
+	fmt.Println(hugo.Ativo)
+}
